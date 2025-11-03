fd6265d64d704506ea726252e89af12339e93f5d
Vancha <tjipkevanderheide90@gmail.com>
2025-11-03 16:29:08 +0100

    Added constant for rom start address

--- src/constants.rs
+++ src/constants.rs
@@ -6,3 +6,5 @@ pub const DISPLAY_HEIGHT: usize = 32;
 pub const RAM_SIZE: usize = 4096;
 /// How many cycles the cpu advances for every frame. This decides how fast the cpu will run
 pub const CYCLES_PER_FRAME: usize = 5;
+/// For the regular chip 8 roms
+pub const ROM_START_ADDRESS: u16 = 0x200;
