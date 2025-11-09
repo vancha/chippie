5ab27b77775c498d1d134b3ed9f9ed9eee358be3
Elijah Shvarov <illya.shvarov@gmail.com>
2025-11-09 10:12:09 +0100

    Add a unit test for the 'FX55' opcode

--- src/constants.rs
+++ src/constants.rs
@@ -3,8 +3,10 @@ pub const DISPLAY_WIDTH: usize = 64;
 /// The height of the display in pixels
 pub const DISPLAY_HEIGHT: usize = 32;
 /// The size of ram in bytes
-pub const RAM_SIZE: usize = 4096;
+pub const RAM_SIZE: u16 = 4096;
 /// How many cycles the cpu advances for every frame. This decides how fast the cpu will run
 pub const CYCLES_PER_FRAME: usize = 5;
 /// For the regular chip 8 roms
 pub const ROM_START_ADDRESS: u16 = 0x200;
+/// Amount of registers CHIP-8 has
+pub const NUM_REGISTERS: u8 = 16;
