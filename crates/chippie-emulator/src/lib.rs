4a27366bf710f8a1944537239a1e39b383578754
Elijah Shvarov <illya.shvarov@gmail.com>
2025-11-14 14:44:07 +0100

    Hide unnecessary modules of the 'chippie-emulator' crate

--- crates/chippie-emulator/src/lib.rs
+++ crates/chippie-emulator/src/lib.rs
@@ -1,16 +1,17 @@
 #![allow(unused_variables, dead_code)]
 
 ///This holds all of the constants (written in capital letters in the code)
-pub mod constants;
+mod constants;
+///An overview of all instructions in the chip 8 instruction set architecture
+mod instruction;
+///A data structure modeling ram
+mod ram;
+///The registers for the chip8 cpu
+mod registers;
+///The stack that is used in the cpu
+mod stack;
+
 ///Handles the fetch, decode execute cycle
 pub mod cpu;
-///An overview of all instructions in the chip 8 instruction set architecture
-pub mod instruction;
-///A data structure modeling ram
-pub mod ram;
-///The registers for the chip8 cpu
-pub mod registers;
 ///Holds the data loaded from disk
 pub mod rombuffer;
-///The stack that is used in the cpu
-pub mod stack;
